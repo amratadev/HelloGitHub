f54ffc851f48e780c78d8a58662eda2d4f3e3163
amrata.deokar <amrata.deokar@gmail.com>
2025-12-01 21:49:54 +0530

    Added second project code

---
+++ string_manipulation.py
@@ -0,0 +1,9 @@
+#print() helps us to show output on the console.
+print("Hello world\nHello world\nHello world")
+print("hello"+" "+"Amrata")
+  #print("Hello")   # ❌ Wrong: extra space before print
+print("A", "B", "C",sep="-")
+print("Hello",end="!!!")
+print("World")
+print() #blank line
+
