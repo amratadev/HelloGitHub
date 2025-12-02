2f4a57aeddc6554bd536b4c6680b4ecc8e05f847
amrata.deokar <amrata.deokar@gmail.com>
2025-12-02 10:19:45 +0530

    Added variable examples in var_in_python.py

---
+++ var_in_python.py
@@ -0,0 +1,10 @@
+name=input("What is your name?")
+print(name)
+name="Jenny"
+print(name)#as the name suggest Variable values can vary
+a=1 #Dynamically typed
+b="Payal"
+print(a)
+print(b)
+length=len(name)
+print(length)
