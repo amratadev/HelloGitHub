9c699d4314b227eb0258bc99cb8c3dbc53b0911e
amrata.deokar <amrata.deokar@gmail.com>
2025-12-04 21:24:44 +0530

    Added membership_operators.py to Python-Basic-to-Advance repo

---
+++ membership_operators.py
@@ -0,0 +1,8 @@
+str = 'Jenny'
+print('t' not in str)
+List=["Amrata","Hrutuja"]
+print("Amrata" in List)
+Tuple=(1,2,3)
+print(1 not in Tuple)
+Dict={"name":"Hrutuja","age":25}
+print("Hrutuja" in Dict)
