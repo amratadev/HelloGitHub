023cc29c012a21cc0dd8a065b01f83f49090f9c6
amrata.deokar <amrata.deokar@gmail.com>
2025-12-05 21:14:40 +0530

    Added if_else.py to Python-Basic-to-Advance repo

---
+++ if_else.py
@@ -0,0 +1,8 @@
+height=int(input("enter height in feet:"))
+
+if(height>3):
+    print("Buy Token")
+    print("Now You can board the metro" )
+#print("out of if block")
+else:
+    print("No Token required")
