1e1e250ad6334bf1471c78c8109bebcd37a2f570
amrata.deokar <amrata.deokar@gmail.com>
2025-12-06 20:22:16 +0530

    Added nested_elif.py to Python-Basic-to-Advance repo

---
+++ nested_elif.py
@@ -0,0 +1,15 @@
+height = int(input("What is your height in feet: "))
+
+if height >= 3:
+    print("You can ride")
+    age = int(input("What is your age: "))
+    if age < 12:
+        print("Pay 150 Rs")
+    elif age <= 18:
+        print("Pay 250 Rs")
+    else:
+        print("Pay 500 Rs")
+else:
+    print("Cant Ride")
+
+print("Bye")
