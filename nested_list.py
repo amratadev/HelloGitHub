b35415455569aa676d805e00a28b6cdb94352465
amrata.deokar <amrata.deokar@gmail.com>
2025-12-08 18:28:07 +0530

    Added nested_list.py to Python-Basic-to-Advance repo

---
+++ nested_list.py
@@ -0,0 +1,8 @@
+numbers=[-1,10,20,25,[2,3,5],30,45]
+print(len(numbers))
+print(numbers[4])
+print(numbers[4][2])
+print(len(numbers[4]))
+print(numbers[4][len(numbers[4]) - 1])   # → 5
+print(numbers[4][0:3])
+print(numbers[2:3])
