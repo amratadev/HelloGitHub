4be0fbe625b776ccb26f5f361215603d21b76c4c
amrata.deokar <amrata.deokar@gmail.com>
2025-12-16 21:03:00 +0530

    whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,23 @@
+count=1
+while(count <= 5):
+     print(count)
+     count += 1
+     if count==3:
+          break
+else:
+     print("In else block")
+
+        
+list1=[1,2,3,4,5]
+while list1:
+     print("Hi,Jenny")
+     list1.pop()
+
+
+i=1
+while i<1:
+    print(i)
+    i+=1
+else:
+    print("in else block")
+print("out from block")
