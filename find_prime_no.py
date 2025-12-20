d4fe4e662d02a598d03712a37d3b1ab5c0f661c0
amrata.deokar <amrata.deokar@gmail.com>
2025-12-20 18:12:45 +0530

    find_prime_no.py

---
+++ find_prime_no.py
@@ -0,0 +1,16 @@
+def prime_checker(number):
+    is_prime=True
+    if number==1:
+        is_prime=False
+    for i in range(2,number):
+        if number%i==0:
+             is_prime=False
+    if is_prime:
+        print("It is a prime number")
+    else:
+        print("Not a prime number")       
+n=int(input("enter a number:"))
+prime_checker(n)
+        
+
+        
