612e653f2dcad42cf6ede6fae65385a4940fbcff
amrata.deokar <amrata.deokar@gmail.com>
2025-12-29 16:29:15 +0530

    dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -18,6 +18,14 @@ phone_no['Shyam']={'Shyam_home':9152,'Shyam_work':5233}
 print(phone_no['Shyam']['Shyam_work']) 
 del phone_no['Ram']
 print(phone_no)
-#print(phone_no.pop('Shyam'))
-print(phone_no.popitem())
+#print(phone_no.pop('Shyam'))particular item delete
+print(phone_no.popitem())#random delete
 print(phone_no)
+print(phone_no.keys())
+print(phone_no.values())
+print(phone_no.items())
+for i in phone_no.items():
+    print(i)
+phone_no2=phone_no.copy()
+print(phone_no2)
+    
