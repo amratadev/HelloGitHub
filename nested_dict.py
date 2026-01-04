4c69beccead5fd7469569adf1e437dd016af3b11
amrata.deokar <amrata.deokar@gmail.com>
2026-01-04 18:51:52 +0530

    nested_dict.py

---
+++ nested_dict.py
@@ -0,0 +1,23 @@
+student_data=[
+       {
+           "Name":"Ram",
+           "roll_no":10,
+           "age":20,
+           "course":"Python"
+       },
+       {
+           "Name":"Mohan",
+           "roll_no":20,
+           "age":22,
+           "course":"Java",
+           "phone_no":[8695757,6574]
+       }
+]
+print(student_data[1]["phone_no"])
+#nesting a list in dictionary
+#travel_data={
+#    "Gujrat":["Dwarkadhish","Somnath","Statue of unity"],
+#    "Rajasthan":["Jaipur","Udaipur"]
+#}
+#print(travel_data)
+
